cdb081fb149a52d9d11a26fd640b7d6ac5ab7c56
Mohit <67197546+maddyaroraa@users.noreply.github.com>
2021-09-23 22:26:16 +0530

    Delete src/components/button directory

--- src/components/button/button.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import './Button.css'; 
-const Button = (props) => {
-    const {className, onClick, children = 'submit' } = props;
-
-    return <button type="button" onClick={() => onClick(Math.random())} className={`btn btn-primary ${className}`}> {children} </button>;
-}
-export default Button;
